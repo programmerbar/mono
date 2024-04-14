6116151abe9e1f310969de26a957b0316e796550
Ole Magnus Fon Johnsen <me@omfj.no>
2024-04-14 20:28:14 +0200

    💄 Update screen page

--- apps/web/src/routes/screen/+page.svelte
+++ apps/web/src/routes/screen/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	let { data } = $props();
 
-	const MAX = 12;
+	const MAX = 10;
 	const TOTAL_PAGES = Math.ceil(data.products.length / MAX);
 	let page = $state(0);
 
@@ -20,21 +20,21 @@
 <div class="bg-primary min-h-screen flex flex-col p-6">
 	<div class="bg-background p-4 flex-1 rounded-xl flex flex-col shadow-xl border">
 		<div class="mb-10 flex items-center justify-between p-4">
-			<h1 class="text-6xl italic">Vår meny</h1>
+			<h1 class="text-7xl italic font-mono font-medium">Vår meny</h1>
 
 			<p class="text-2xl font-medium">{page + 1} / {TOTAL_PAGES}</p>
 		</div>
 
-		<div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+		<div class="grid grid-cols-1 md:grid-cols-2 gap-4 flex-1 justify-center items-center">
 			{#each data.products.slice(start, end) as { name, producer, price }}
-				<div class="flex items-center justify-between bg-neutral-50 p-6 border-2">
+				<div class="flex items-center justify-between h-full rounded-xl bg-neutral-50 p-6 border-2">
 					<div class="flex flex-col">
-						<p class="text-2xl">{name}</p>
-						<p class="text-gray-700">{producer}</p>
+						<p class="text-4xl">{name}</p>
+						<p class="text-gray-700 text-lg font-mono">{producer}</p>
 					</div>
 
 					<div>
-						<p class="font-medium text-2xl italic">{price} kr</p>
+						<p class="text-4xl font-light italic">{price} kr</p>
 					</div>
 				</div>
 			{/each}
