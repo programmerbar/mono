439ea9fcd7a4d4f3325f3cb8e0595bb946d5d19a
Ole Magnus Fon Johnsen <me@omfj.no>
2024-04-18 15:22:43 +0200

    ✨ Screen animation

--- apps/web/src/routes/screen/+page.svelte
+++ apps/web/src/routes/screen/+page.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+	import { cn } from '$lib/cn';
+	import { fly } from 'svelte/transition';
+
 	let { data } = $props();
 
 	const MAX = 10;
@@ -22,26 +25,44 @@
 </svelte:head>
 
 <div class="bg-primary min-h-screen flex flex-col p-6">
-	<div class="bg-background p-4 flex-1 rounded-xl flex flex-col shadow-xl border">
+	<div class="bg-background p-4 overflow-hidden flex-1 rounded-xl flex flex-col shadow-xl border">
 		<div class="mb-10 flex items-center justify-between p-4">
 			<h1 class="text-7xl italic font-mono font-medium">Vår meny</h1>
 
-			<p class="text-2xl font-medium">{page + 1} / {TOTAL_PAGES}</p>
+			<div class="flex flex-row items-center gap-2">
+				{#each Array.from({ length: TOTAL_PAGES }) as _, i}
+					<div
+						class={cn('w-6 h-6 rounded-full bg-background border-primary border-2', {
+							'bg-primary': i === page
+						})}
+					></div>
+				{/each}
+			</div>
 		</div>
 
-		<div class="grid grid-cols-1 md:grid-cols-2 gap-4 flex-1 justify-center items-center">
-			{#each data.products.slice(start, end) as { name, producer, price }}
-				<div class="flex items-center justify-between h-full rounded-xl bg-neutral-50 p-6 border-2">
-					<div class="flex flex-col">
-						<p class="text-4xl">{name}</p>
-						<p class="text-gray-700 text-lg font-mono">{producer}</p>
-					</div>
+		<div class="relative">
+			{#key page}
+				<div
+					class="absolute w-full grid grid-cols-1 md:grid-cols-2 gap-4 flex-1 justify-center items-center"
+					in:fly={{ x: 50, duration: 500 }}
+					out:fly={{ x: -50, duration: 500 }}
+				>
+					{#each data.products.slice(start, end) as { name, producer, price }}
+						<div
+							class="flex items-center justify-between h-full rounded-xl bg-neutral-50 p-6 border-2"
+						>
+							<div class="flex flex-col">
+								<p class="text-4xl">{name}</p>
+								<p class="text-gray-700 text-lg font-mono">{producer}</p>
+							</div>
 
-					<div>
-						<p class="text-4xl font-light italic">{price} kr</p>
-					</div>
+							<div>
+								<p class="text-4xl font-light italic">{price} kr</p>
+							</div>
+						</div>
+					{/each}
 				</div>
-			{/each}
+			{/key}
 		</div>
 	</div>
 </div>
