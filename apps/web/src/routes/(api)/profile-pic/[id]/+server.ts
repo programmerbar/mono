fd88830c49998a7173c9d7463a18f77ac9f88de8
Ole Magnus Fon Johnsen <me@omfj.no>
2024-04-25 18:29:50 +0200

    🐛 Handle no image

--- apps/web/src/routes/(api)/profile-pic/[id]/+server.ts
+++ apps/web/src/routes/(api)/profile-pic/[id]/+server.ts
@@ -4,6 +4,10 @@ export const GET = async ({ platform, params }) => {
 	const avatarPath = `avatars/${id}`;
 	const avatar = await platform?.env.R2.get(avatarPath);
 
+	if (!avatar) {
+		return new Response(null, { status: 404 });
+	}
+
 	const headers = new Headers(Object.entries(avatar?.httpMetadata ?? {}));
 	headers.set('etag', avatar?.etag ?? '');
 
