51e53b9242ea63394a71dd81116fa6c532d9c4b2
Ole Magnus Fon Johnsen <me@omfj.no>
2024-04-30 23:51:07 +0200

    🔥 Remove unused schemas

--- apps/cms/schemas/index.ts
+++ apps/cms/schemas/index.ts
@@ -3,15 +3,11 @@ import price from "./objects/price";
 import producer from "./producer";
 import product from "./product";
 import productType from "./product-type";
-import profile from "./profile";
-import standPlan from "./stand-plan";
 
 export const schemas = [
   events,
   producer,
   product,
-  profile,
-  standPlan,
   productType,
   price,
 ];
