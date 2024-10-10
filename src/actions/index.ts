5e50c3d719f498df7d2ac619581d97cb79ef916a
Ole Magnus Fon Johnsen <me@omfj.no>
2024-10-10 10:10:02 +0200

    📦 Persist feedback in KV

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,16 +1,25 @@
 import { defineAction } from "astro:actions";
 import { z } from "astro:schema";
 import { sendContactEmail } from "../lib/send-contact-email";
+import crypto from "node:crypto";
 
 export const server = {
   contactUsAction: defineAction({
     input: z.object({
       name: z.string().min(2, { message: "Navn er påkrevd" }),
-      email: z.string().min(2, { message: "E-post er påkrevd" }),
+      email: z.string().email().min(2, { message: "E-post er påkrevd" }),
       message: z.string().min(5, { message: "Meldingen må være minst 5 tegn" }),
     }),
-    handler: async ({ name, email, message }) => {
-      await sendContactEmail(name, email, message);
+    handler: async ({ name, email, message }, ctx) => {
+      const id = crypto.randomUUID();
+      await ctx.locals.runtime.env.FEEDBACK_KV.put(
+        `feedback:${id}`,
+        JSON.stringify({ name, email, message }),
+      );
+
+      const apiKey = ctx.locals.runtime.env.RESEND_API_KEY;
+
+      await sendContactEmail(name, email, message, apiKey);
     },
   }),
 };
