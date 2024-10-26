a6fb7222bf55eaca4af30ddb81554de3ce51d947
Ole Magnus Fon Johnsen <me@omfj.no>
2024-10-26 18:35:41 +0200

    📂 Add loading state

--- apps/www/src/routes/portal/status/+page.svelte
+++ apps/www/src/routes/portal/status/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
-	import { Beer, Lock, Users } from 'lucide-svelte';
+	import { Beer, Lock, Users, Loader } from 'lucide-svelte';
 	import { cn } from '$lib/cn';
 	import { enhance } from '$app/forms';
 
 	let { data } = $props();
+	let isUpdating = $state(false);
+	let newStatus = $state(data.status);
 
 	const statusButtons = [
 		{ status: 1, Icon: Beer, label: 'Åpnet', color: 'green-500' },
@@ -15,15 +17,36 @@
 <div class="flex flex-col bg-background gap-2 max-w-[300px] mx-auto p-4 rounded-lg border">
 	{#each statusButtons as { status, Icon, label, color }}
 		{@const isSelected = data.status === status}
-		<form method="post" use:enhance>
+		<form
+			method="post"
+			use:enhance={() => {
+				isUpdating = true;
+				newStatus = status;
+
+				return async ({ update }) => {
+					await update();
+					isUpdating = false;
+				};
+			}}
+		>
 			<input type="hidden" name="status" value={status} />
 			<button
-				class={cn('w-full rounded-lg border p-4 bg-white flex items-center justify-center gap-2', {
-					['bg-' + color]: isSelected,
-					'text-white': isSelected && status === 0
-				})}
+				class={cn(
+					'w-full rounded-lg border p-4 bg-white hover:bg-gray-200 flex transition-colors items-center justify-center gap-2 disabled:cursor-not-allowed disabled:opacity-60',
+					{
+						'bg-green-500 hover:bg-green-600': isSelected && status === 1,
+						'bg-orange-400 hover:bg-orange-500': isSelected && status === 2,
+						'bg-red-500 text-white hover:bg-red-600': isSelected && status === 0
+					}
+				)}
+				disabled={isUpdating}
 			>
-				<Icon class="size-5" />
+				{#if isUpdating && newStatus === status}
+					<Loader class="size-5 animate-spin" />
+				{:else}
+					<Icon class="size-5" />
+				{/if}
+
 				{label}
 			</button>
 		</form>
