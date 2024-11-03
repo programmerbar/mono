93778740d22e514fb1bb0c220552509e70e4301f
Ole Magnus Fon Johnsen <me@omfj.no>
2024-11-03 02:22:22 +0100

    ❤️ Add wishlist

--- apps/www/src/lib/components/app/meny/ProductPreview.svelte
+++ apps/www/src/lib/components/app/meny/ProductPreview.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import { urlFor } from '$lib/api/sanity/image';
+	import { Heart } from 'lucide-svelte';
 	import type { filterProducts } from '$lib/filter-products';
 	import type { FilterState } from '$lib/states/filter-state.svelte';
+	import { WishlistState } from '$lib/states/wishlist-state.svelte';
 
 	type Props = {
 		product: ReturnType<typeof filterProducts>[number];
@@ -9,31 +11,52 @@
 	};
 
 	let { product = $bindable(), filterState = $bindable() }: Props = $props();
+	let wishlist = new WishlistState();
+
+	let isWishlisted = $derived.by(() => wishlist.products.includes(product._id));
+
+	const handleHeartClick = () => {
+		if (isWishlisted) {
+			wishlist.remove(product._id);
+		} else {
+			wishlist.add(product._id);
+		}
+	};
 </script>
 
-<a class="group" href="/produkt/{product._id}">
-	<div
-		class="relative flex h-full flex-col overflow-hidden rounded-xl border-2 bg-background shadow-md"
-	>
-		<div class="relative border-b-2">
-			{#if product.isSoldOut}
-				<div
-					class="absolute left-0 top-0 rounded-br bg-red-500 px-2 py-1 text-xs font-semibold text-white"
-				>
-					Utsolgt
-				</div>
-			{/if}
-
-			{#if product.image}
-				<img
-					src={urlFor(product.image).width(500).height(500).url()}
-					alt={product.name}
-					class="h-48 w-full bg-white object-contain"
-				/>
-			{:else}
-				<div class="h-48 w-full bg-gray-200"></div>
-			{/if}
+<div
+	class="relative flex h-full flex-col overflow-hidden rounded-xl border-2 bg-background shadow-md"
+>
+	<div class="relative border-b-2">
+		<div class="absolute right-0 top-0 z-10 rounded-bl bg-white px-2 py-1 text-xs font-semibold">
+			<button onclick={handleHeartClick}>
+				{#if isWishlisted}
+					<Heart class="fill-red-500 stroke-red-200" />
+				{:else}
+					<Heart class="stroke-red-500" />
+				{/if}
+			</button>
 		</div>
+
+		{#if product.isSoldOut}
+			<div
+				class="absolute left-0 top-0 rounded-br bg-red-500 px-2 py-1 text-xs font-semibold text-white"
+			>
+				Utsolgt
+			</div>
+		{/if}
+
+		{#if product.image}
+			<img
+				src={urlFor(product.image).width(500).height(500).url()}
+				alt={product.name}
+				class="h-48 w-full bg-white object-contain"
+			/>
+		{:else}
+			<div class="h-48 w-full bg-gray-200"></div>
+		{/if}
+	</div>
+	<a class="group" href="/produkt/{product._id}">
 		<div class="flex flex-1 flex-col p-2">
 			<h2 class="text-lg font-semibold group-hover:underline">{product.name}</h2>
 
@@ -53,5 +76,5 @@
 				<p class="mt-auto text-lg font-semibold">{product.priceList.ordinary} kr</p>
 			{/if}
 		</div>
-	</div>
-</a>
+	</a>
+</div>
