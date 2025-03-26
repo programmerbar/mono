7f79b5d2682fb95ca4f2e65e2cbf9f377874f997
Ole Magnus Fon Johnsen <me@omfj.no>
2025-03-26 21:06:50 +0100

    Fix .json ext

--- apps/www/scripts/add-invitation.ts
+++ apps/www/scripts/add-invitation.ts
@@ -11,7 +11,7 @@ type Env = {
 // Example "tsx ./apps/www/scripts/add-invitation.ts "some@email.com""
 const main = async () => {
 	const { env } = await getPlatformProxy<Env>({
-		configPath: './apps/www/wrangler.json',
+		configPath: './apps/www/wrangler.jsonc',
 		persist: {
 			path: './apps/www/.wrangler/state/v3'
 		}
