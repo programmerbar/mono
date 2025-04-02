e967cce1b2eb755a32ff12c62f41c389be82bead
Gard.Kalland <github.blunderer148@passmail.net>
2025-04-02 18:03:30 +0200

    🔐admin feature

--- apps/www/src/routes/portal/arrangementer/[id]/+page.server.ts
+++ apps/www/src/routes/portal/arrangementer/[id]/+page.server.ts
@@ -2,65 +2,72 @@ import { error, fail, redirect } from '@sveltejs/kit';
 import type { Actions, PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ locals, params }) => {
-	const event = await locals.eventService.findFullEventById(params.id);
+  const event = await locals.eventService.findFullEventById(params.id);
 
-	if (!event) {
-		throw error(404, 'Event not found');
-	}
+  if (!event) {
+    throw error(404, 'Event not found');
+  }
 
-	return {
-		event
-	};
+  return {
+    event
+  };
 };
 
 export const actions: Actions = {
-	delete: async ({ params, locals }) => {
-		await locals.eventService.delete(params.id);
-		throw redirect(303, '/portal/arrangementer');
-	},
-	join: async ({ request, locals }) => {
-		if (!locals.user) {
-			return fail(401, {
-				message: 'Not logged in'
-			});
-		}
+  delete: async ({ params, locals }) => {
+    if (locals.user?.role === 'board') {
 
-		const formData = await request.formData();
-		const shiftId = formData.get('shiftId');
-		if (!shiftId || typeof shiftId !== 'string') {
-			return fail(400, {
-				message: 'Missing shiftId'
-			});
-		}
+      await locals.eventService.delete(params.id);
+      throw redirect(303, '/portal/arrangementer');
+    }
 
-		await locals.eventService.createUserShift({
-			shiftId,
-			userId: locals.user.id,
-			status: 'accepted'
-		});
+    return fail(401, {
+      message: 'Unauthorized'
+    });
+  },
+  join: async ({ request, locals }) => {
+    if (!locals.user) {
+      return fail(401, {
+        message: 'Not logged in'
+      });
+    }
 
-		return { success: true };
-	},
-	leave: async ({ request, locals }) => {
-		if (!locals.user) {
-			return fail(401, {
-				message: 'Not logged in'
-			});
-		}
+    const formData = await request.formData();
+    const shiftId = formData.get('shiftId');
+    if (!shiftId || typeof shiftId !== 'string') {
+      return fail(400, {
+        message: 'Missing shiftId'
+      });
+    }
 
-		const formData = await request.formData();
-		const shiftId = formData.get('shiftId');
-		if (!shiftId || typeof shiftId !== 'string') {
-			return fail(400, {
-				message: 'Missing shiftId'
-			});
-		}
+    await locals.eventService.createUserShift({
+      shiftId,
+      userId: locals.user.id,
+      status: 'accepted'
+    });
 
-		await locals.eventService.deleteUserShift({
-			shiftId,
-			userId: locals.user.id
-		});
+    return { success: true };
+  },
+  leave: async ({ request, locals }) => {
+    if (!locals.user) {
+      return fail(401, {
+        message: 'Not logged in'
+      });
+    }
 
-		return { success: true };
-	}
+    const formData = await request.formData();
+    const shiftId = formData.get('shiftId');
+    if (!shiftId || typeof shiftId !== 'string') {
+      return fail(400, {
+        message: 'Missing shiftId'
+      });
+    }
+
+    await locals.eventService.deleteUserShift({
+      shiftId,
+      userId: locals.user.id
+    });
+
+    return { success: true };
+  }
 };
