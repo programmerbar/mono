93d4901f444b79bb34a2bf10497ff0302c1c1a38
Gard <128602894+GardKalland@users.noreply.github.com>
2025-04-30 23:57:33 +0200

    redirect fix

--- apps/www/src/routes/portal/arrangementer/ny/+page.server.ts
+++ apps/www/src/routes/portal/arrangementer/ny/+page.server.ts
@@ -1,6 +1,10 @@
 import type { PageServerLoad } from './$types';
+import { redirect } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async ({ locals }) => {
+	if (!locals.user || locals.user.role !== 'board') {
+		throw redirect(303, '/portal');
+	}
 	const users = await locals.userService.findAll().then((users) =>
 		users.map((user) => ({
 			label: user.name,
