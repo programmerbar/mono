de61a0d1324d38b0d7a9671961d45320190ac272
Ole Magnus Fon Johnsen <me@omfj.no>
2025-08-13 20:22:52 +0200

    Better use of mode

--- apps/www/vite.config.ts
+++ apps/www/vite.config.ts
@@ -2,13 +2,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
+export default defineConfig(({ command, mode }) => ({
 	plugins: [sveltekit(), tailwindcss()],
 	resolve: {
-		alias: import.meta.env.PROD
-			? {
-					'react-dom/server': 'react-dom/server.edge'
-				}
-			: undefined
+		alias:
+			mode === 'production'
+				? {
+						'react-dom/server': 'react-dom/server.edge'
+					}
+				: undefined
 	}
-});
+}));
