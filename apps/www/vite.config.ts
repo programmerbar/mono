db8a9dff729d685d377ce1ec17f860f580e5cdcd
Ole Magnus Fon Johnsen <me@omfj.no>
2025-08-13 20:18:58 +0200

    Use react dom server

--- apps/www/vite.config.ts
+++ apps/www/vite.config.ts
@@ -3,5 +3,12 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), tailwindcss()]
+	plugins: [sveltekit(), tailwindcss()],
+	resolve: {
+		alias: import.meta.env.PROD
+			? {
+					'react-dom/server': 'react-dom/server.edge'
+				}
+			: undefined
+	}
 });
