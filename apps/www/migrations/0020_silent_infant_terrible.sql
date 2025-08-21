a0cae4b9f7b2b620e15e028b8aed598f938de128
GardKalland <github.blunderer148@passmail.net>
2025-08-21 19:48:06 +0200

    migration fix

---
+++ apps/www/migrations/0020_silent_infant_terrible.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `images` DROP COLUMN `original_name`;--> statement-breakpoint
+ALTER TABLE `images` DROP COLUMN `mime_type`;
--- apps/www/migrations/0027_nice_bushwacker.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `producers` (
-	`id` text PRIMARY KEY NOT NULL,
-	`name` text NOT NULL,
-	`image_id` text,
-	`created_at` text NOT NULL,
-	`updated_at` text NOT NULL
-);
