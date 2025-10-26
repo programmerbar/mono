70c50d049f0056384d527ffc4f46739d28a3d576
Ole Magnus Fon Johnsen <me@omfj.no>
2025-10-26 17:55:34 +0100

    chore: remove unused files and deps

--- programmerbar-web/src/lib/types.ts
+++
@@ -1,9 +0,0 @@
-import type { RouteId, Pathname } from '$app/types';
-
-export type ExternalLink = `http${string}` | `mailto:${string}`;
-export type InternalLink = RouteId | Pathname;
-
-export const isExternalLink = (href: string): href is ExternalLink =>
-	href.startsWith('http') || href.startsWith('mailto:');
-
-export const isInternalLink = (href: string): href is InternalLink => !isExternalLink(href);
