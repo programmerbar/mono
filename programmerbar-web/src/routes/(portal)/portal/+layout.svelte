fb3c09ed978509ad19c1f665cd3727c48ce3905d
Ole Magnus Fon Johnsen <me@omfj.no>
2025-10-27 07:17:52 +0100

    feat: add footer to portal layout

--- programmerbar-web/src/routes/(portal)/portal/+layout.svelte
+++ programmerbar-web/src/routes/(portal)/portal/+layout.svelte
@@ -24,10 +24,19 @@
 	/>
 
 	<div class="min-w-0 flex-1 lg:ml-64">
-		<main class="min-w-0 flex-1 p-4 sm:p-6 lg:p-8">
+		<main class="mb-32 min-w-0 flex-1 p-4 sm:px-6 sm:pt-6 lg:px-8 lg:pt-8">
 			<div class="mx-auto max-w-5xl min-w-0">
 				{@render children()}
 			</div>
 		</main>
+
+		<footer class="dark:border-portal-border border-t border-gray-200 p-20 text-center">
+			<p class="text-sm text-gray-600 dark:text-gray-400">
+				Har du problemer med siden? Ta kontakt med <a
+					href="mailto:web@programmerbar.no"
+					class="text-primary hover:underline">web@programmerbar.no</a
+				>
+			</p>
+		</footer>
 	</div>
 </div>
